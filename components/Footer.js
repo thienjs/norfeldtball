0cd1bc72d7184cb4da4f6387918368a2f4f16501
thienjs <thien.tran6@gmail.com>
2021-04-11 21:22:06 -0400

    footer change

--- components/Footer.js
+++ components/Footer.js
@@ -4,27 +4,8 @@ const Footer = ({ fullWidth }) => {
   const y = d.getFullYear()
   const from = +BLOG.since
   return (
-    <div
-      className={`mt-6 flex-shrink-0 m-auto font-noEmoji w-full text-gray-500 dark:text-gray-500 transition-all ${
-        !fullWidth ? 'max-w-2xl px-4' : 'px-4 md:px-24'
-      }`}
-    >
-      <hr className="border-gray-200 dark:border-gray-600" />
-      <div className="my-4 text-sm leading-6 font-medium">
-        <p>
-          <a
-            href="https://github.com/craigary/nobelium"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Nobelium
-          </a>{' '}
-          is built with ♥ and ⚛ Next.js. Proudly deployed on ▲Vercel.
-        </p>
-        <p>
-          © {BLOG.author} {from === y || !from ? y : `${from} - ${y}`}
-        </p>
-      </div>
+    <div>
+      <p>under construction</p>
     </div>
   )
 }
